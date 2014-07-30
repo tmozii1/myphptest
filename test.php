a8d66b2b8b9b578b5f3f3c985c7ad1dd201049a0
tmozii1 <tmozii1@gmail.com>
2014-07-30 16:14:42 +0900

    select query

--- test.php
+++ test.php
@@ -3,7 +3,11 @@
 	include("include/mylib.inc");
 	$con = db_con();
 
-	echo $con;
+	$qry1 = "select * from mytable";
+	$res1 = mysql_query($qry1, $con);
+
+	$num1 = mysql_num_rows($res1);
+	echo $num1;
 
 	mysql_close($con);
 
