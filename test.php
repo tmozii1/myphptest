b1bbbbf77f3861cec209b936015d00fafb21f915
tmozii1 <tmozii1@gmail.com>
2014-07-30 15:54:38 +0900

    php database access

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
-	$con = "aaa";
+	$host="localhost";
+	$user="root";
+	$pwd="bitnami";
+
+	$con = mysql_connect($host, $user, $pwd);
+	mysql_select_db('mydb', $con);
+	
 	echo $con;
+	
+	mysql_close($con);
+
 ?>
