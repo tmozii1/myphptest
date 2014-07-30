a81cfca787b5528908da4fc17720a0e4198e992c
tmozii1 <tmozii1@gmail.com>
2014-07-30 16:37:12 +0900

    field count

--- test.php
+++ test.php
@@ -7,12 +7,20 @@
 	$res1 = mysql_query($qry1, $con);
 
 	$num1 = mysql_num_rows($res1);
+	$field_count = mysql_num_fields($res1);
+
+	echo "field count : ".$field_count."<br/>";
 
 	for($i=0;$i<$num1;$i++){
 		$arr = mysql_fetch_array($res1);
-		echo $i." : ".$arr[0]."<br/>";
+		for($j=0;$j<$field_count;$j++){
+			echo $arr[$j];
+			if($j < $field_count-1){
+				echo ", ";
+			}
+		}
+		echo "<br/>";
 	}
-
 	mysql_close($con);
 
 ?>
