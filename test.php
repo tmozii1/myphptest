d36de5f98b03d1642ec06f2740777c8a9397a8ed
tmozii1 <tmozii1@gmail.com>
2014-07-30 16:12:33 +0900

    database info seperate

--- test.php
+++ test.php
@@ -1,13 +1,10 @@
 <?php
-	$host="localhost";
-	$user="root";
-	$pwd="bitnami";
 
-	$con = mysql_connect($host, $user, $pwd);
-	mysql_select_db('mydb', $con);
-	
+	include("include/mylib.inc");
+	$con = db_con();
+
 	echo $con;
-	
+
 	mysql_close($con);
 
 ?>
