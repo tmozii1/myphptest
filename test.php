c9e3ec081c8871ead6c6c065a310c55cb2895a02
tmozii1 <tmozii1@gmail.com>
2014-07-30 16:24:55 +0900

    select and fetch data

--- test.php
+++ test.php
@@ -7,7 +7,11 @@
 	$res1 = mysql_query($qry1, $con);
 
 	$num1 = mysql_num_rows($res1);
-	echo $num1;
+
+	for($i=0;$i<$num1;$i++){
+		$arr = mysql_fetch_array($res1);
+		echo $i." : ".$arr[0]."<br/>";
+	}
 
 	mysql_close($con);
 
